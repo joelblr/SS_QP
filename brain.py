5ef7258f49365d72ec589d16cd5f5bc69874f0b8
joelblr <joel022003@gmail.com>
2024-08-26 21:03:19 +0530

    added files

---
+++ brain.py
@@ -0,0 +1,205 @@
+import os, sys
+import csv, random
+from datetime import datetime
+
+
+def findChapterFreq(files) :
+    #FIXME: use caching, as this will be done again n again.
+    # chapter_freq = {}
+    # for row in lines :
+    #     tot_chap[row[]]
+    pass
+
+
+
+
+""" partName: str, freq: int, unique-KeyIndex|ValueIndex: int, limit: int = 1"""
+def getPart(fileName, freq, uniKeyIdx, uniValIdx, limit, partId) :
+
+    """
+    TODO: make unique based on multi-Column
+            like, year + chapter + pgNum
+
+    #XXX: handle Errors
+    """
+ 
+    with open(fileName) as csvf :
+        """ (year, chapter, question) """
+        lines = [tuple(line) for line in csv.reader(csvf)]
+
+    used_ids["total"][partId] = len(lines)-1
+    cur_used_ids = {line[uniKeyIdx] : [] for line in lines}
+    res = []
+    for i in range(freq) :
+
+        flag = True
+        while flag :
+
+            ques = random.choices(lines[1:])[0]
+            key1 = ques[uniKeyIdx]
+            key2 = ques[uniValIdx]
+
+            if (len(cur_used_ids[key1]) >= limit) :
+                cur_used_ids[key1] = []
+
+            if (len(used_ids[partId]) == used_ids["total"][partId]) :
+                used_ids[partId] = set()
+                used_ids["done"][partId] = True
+
+            if (key2 not in used_ids[partId]) :
+                res.append(ques)
+                used_ids[partId].add(key2)
+                cur_used_ids[key1].append(key2)
+                flag = False
+
+    # for i, t in enumerate(res) :
+    #     print(i, t)
+
+    if (len(used_ids[partId]) == used_ids["total"][partId]) :
+        used_ids[partId] = set()
+        used_ids["done"][partId] = True
+
+    # List[tuple(str, str, str), ]
+    return res
+
+
+""" placeholder_prefix: str, val_format_type: str, csv_object: List[List[str, str]]
+    -> List[List[str, str]]
+"""
+def getJsonFormat(placeholder_prefix, val_format_type, csv_object) :
+    json_res = []
+
+    # for idx, row in enumerate(csv_object) :
+    #     json_res.append([
+    #         f"{placeholder_prefix}{idx+1}",
+    #         val_format_type.format(*row)
+    #     ])
+    for i in range(len(placeholder_prefix)) :
+        for idx, row in enumerate(csv_object) :
+            json_res.append([
+                f"{placeholder_prefix[i]}{idx+1}",
+                val_format_type[i].format(*row)
+            ])
+
+    # for key, val in json_res :
+    #     print(key, val)
+
+    return json_res
+
+
+""" obj: dict(str: str), new_obj: dict(str: str) """
+def updateObject(obj, new_obj) :
+    for key, val in new_obj :
+        obj[key] = val
+
+
+""" obj: dict[str, str] -> None """
+def getOsDetails() :
+    # Windows
+    osc['bs'] = "\\"
+    osc['os'] = 'nt'
+    if os.name == "posix":  # Linux or macOS
+        osc['bs'] = "/"
+        osc['os'] = 'posix'
+
+
+""" relative_path: str -> str """
+def resource_path(relative_path) :
+
+    try :
+        base_path = sys._MEIPASS
+    except Exception :
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+""" relative_path: str, prefix_dir: str """
+def openDoc(relative_path, prefix_dir) :
+
+    file_path = resource_path(rectifiedPath(prefix_dir, relative_path))
+    # print(file_path)
+
+    if os.path.exists(file_path) :
+        if osc["os"] == "nt" :      # Windows
+            os.startfile(file_path)
+        elif osc["os"] == "posix":  # Linux or macOS
+            os.system("xdg-open " + file_path)
+    else :
+        print("File not found")
+
+
+""" doc: DocxTemplate, folder_path: str, prefix_tpl: str -> str """
+def saveFile(doc, folder_path, prefix_tpl) :
+
+    # newfile = prefix_tpl + datetime.now().strftime("%S%M") + ".docx"
+    newfile = prefix_tpl + ".docx"
+    newfile = resource_path(rectifiedPath(folder_path, newfile))
+
+    if not os.path.exists(folder_path) :
+        if osc["os"] == "nt" :      # Windows
+            os.system("mkdir " + folder_path)
+        elif osc["os"] == "posix":  # Linux or macOS
+            os.system("mkdir -p " + folder_path)
+    doc.save(newfile)
+
+    return newfile
+
+
+""" folder_path: str -> None """
+def open_file_explorer(folder_path) :
+
+    if os.path.exists(folder_path) :
+        if osc["os"] == "nt" :      # Windows
+            os.system("start explorer " + folder_path)
+        elif osc["os"] == "posix":  # Linux or macOS
+            os.system("xdg-open " + folder_path)
+    else :
+        print("File not found")
+
+
+"""" args: List[str] -> str """
+def rectifiedPath(*args: list[str]) -> str :
+    return osc['bs'].join(args)
+
+#----- CACHE ---------------------------------------
+# os-config
+osc = {}
+getOsDetails()
+
+
+# contains set of used-IDs for a given Section/Part A->0, B->1... G->6
+used_ids = {i : set() for i in range(7)}
+
+# total Qs in each part
+used_ids["total"] = [0 for i in range(7)]
+
+used_ids["done"] = [False for i in range(7)]
+# used_ids["done"][1] = True  #part_B
+
+# # GET as INPUT-GUI
+inputs = {
+    "prefix_tpl" : input("Enter the Folder Name: ")+"_",
+    "year" : "2025*",
+    "standard" : "SENIOR 1",
+    "tpl" : "SS_Template2.docx",
+    "type" : "main",
+    "saveas" : "",#as a separate dir for the test papers generated per one run
+}
+
+myDirs = {
+    "pwd" : '.',
+    "tpl" : "templates",
+    ".csv" : resource_path(rectifiedPath("data-sets", inputs["standard"], inputs["type"])),
+    "mqp" : resource_path(rectifiedPath("model-paper", inputs["prefix_tpl"][:-1])),
+}
+
+parts_map = {
+    0 : (10, 1, 0, 5),
+    1 : (10, 2, 0, 5),
+    2 : (10, 2, 0, 1),
+    3 : (6, 2, 0, 1),
+    4 : (3, 2, 0, 1),
+    5 : (2, 2, 0, 1),
+    6 : (1, 0, 0, 1),
+}
---
+++ main.py
@@ -0,0 +1,62 @@
+"""
+TODO: Run a While-Infinite Loop, Cache all used Qs to form N Unique papers
+"""
+
+# from docx import Document
+from docxtpl import DocxTemplate
+from brain import *
+from random import randint
+
+
+pre_idx = 1
+# to create as many unique papers as possible
+while not all(used_ids["done"]) :
+
+    # GET some inputs from GUI <standard, whichLessonsToFocus...>
+    context = {
+        "year" : inputs['year'],
+        "standard" : inputs['standard'],
+    }
+
+    tpl_name = resource_path(rectifiedPath(myDirs["tpl"], inputs["tpl"]))
+    doc = DocxTemplate(tpl_name)
+
+
+    csv_file, part_qs = [], []
+    csv_name = resource_path(rectifiedPath(myDirs[".csv"], 'Part_A.csv'))
+    csv_file = getPart(csv_name, *parts_map[0], 0)
+    part_qs = getJsonFormat(["part_A"], ["{3}"], csv_file)
+    updateObject(context, part_qs)
+
+    csv_file, part_qs = [], []
+    csv_name = resource_path(rectifiedPath(myDirs[".csv"], 'Part_B2.csv'))
+    csv_file = getPart(csv_name, *parts_map[1], 1)
+    part_qs = getJsonFormat(["part_B", "part_B1"], ["{4} ({2} -> {3}) [{1}]", "{5}"], csv_file)
+    updateObject(context, part_qs)
+
+
+    for i, c in enumerate("CDEF") :
+        csv_file, part_qs = [], []
+        csv_name = resource_path(rectifiedPath(myDirs[".csv"], f'Part_{c}.csv'))
+        csv_file = getPart(csv_name, *parts_map[i+2], i+2)
+        part_qs = getJsonFormat([f"part_{c}"], ["{4} ({2} -> {3}) [{1}]"], csv_file)
+        updateObject(context, part_qs)
+
+    csv_file, part_qs = [], []
+    csv_name = resource_path(rectifiedPath(myDirs[".csv"], 'Part_G.csv'))
+    csv_file = getPart(csv_name, *parts_map[6], 6)
+    part_qs = getJsonFormat(["part_G"], ["{2} [{1}]"], csv_file)
+    updateObject(context, part_qs)
+
+
+    doc.render(context)
+
+    newFileName = saveFile(doc, myDirs["mqp"], inputs['prefix_tpl'] + f"{pre_idx}")
+
+    # openDoc(newFileName, myDirs["mqp"])
+    pre_idx += 1
+
+print("Successfully Downloaded all the Possible Model-Papers")
+print("Check it Out in:")
+print("\t", myDirs["mqp"])
+open_file_explorer(myDirs["mqp"])
