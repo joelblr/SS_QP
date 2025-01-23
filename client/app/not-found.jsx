35d0ebe6f4c51b9203fd8f487b10e9a5c031edb7
joelblr <joel022003@gmail.com>
2025-01-23 18:51:24 +0530

    added custom-404 page

---
+++ client/app/not-found.jsx
@@ -0,0 +1,14 @@
+// pages/404.js
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+      <h1 className="text-6xl font-bold text-red-500">404</h1>
+      <p className="mt-4 text-xl text-gray-700">Oops! Page not found.</p>
+      <Link href="/" className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300">
+          Go back home
+      </Link>
+    </div>
+  );
+};
